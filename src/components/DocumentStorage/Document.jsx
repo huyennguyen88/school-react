95562ee85821c03e15557b0a858415ec9cf8c04a
huyennguyen88 <judyhuyen17498@gmail.com>
2019-12-16 09:16:04 +0700

    no

---
+++ src/components/DocumentStorage/Document.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Document extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default Document 
--- src/components/DocumentStorage/PublicStorage.jsx
+++ src/components/DocumentStorage/PublicStorage.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from 'react'
 import LeftSideBar from './LeftSideBar';
+
+import FileViewer from 'react-file-viewer';
+import { CustomErrorComponent } from 'custom-error';
 class PublicStorage extends Component {
+
     render() {
+        const file = 'http://www.bavtailor.com/wp-content/uploads/2018/10/Lorem-Ipsum.pdf'
+        const type = 'pdf'
         return (
-    
-                <div className="wrapper">
-                    <LeftSideBar/>
-                    <div id="content">
-                       <h1>Public storage</h1>
-                    </div>
+
+            <div className="wrapper">
+                <LeftSideBar />
+                <div id="content">
+                    <h1>Public storage</h1>
+                    <FileViewer
+                        fileType={type}
+                        filePath={file}
+                        errorComponent={CustomErrorComponent}
+                         />
                 </div>
+            </div>
         )
     }
 }
